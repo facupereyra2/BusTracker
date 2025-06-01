9dd32bd68eda8f7ac4240fd6262feaff6eeea19e
Facundo Pereyra <facupereyra3@gmail.com>
2025-06-01 18:33:23 -0300

    fix port

--- Backend/index.js
+++ Backend/index.js
@@ -202,4 +202,6 @@ app.get('/distance', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Servidor en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+
